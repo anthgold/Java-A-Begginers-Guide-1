669bd63155a0ef00d177cd81e6e8be4acf1849ba
Anthony Gold <anthgold@gmail.com>
2016-04-19 20:24:24 -0700

    adds the ForDemo program, which uses a for loop to count from 0 to 4

---
+++ ForDemo.java
@@ -0,0 +1,14 @@
+// Demonstrates the for loop
+//
+// File is called ForDemo.java.
+
+class ForDemo {
+  public static void main(String args[]) {
+    int count;
+
+    for(count = 0; count < 5; count++)
+      System.out.println("This is count: " + count);
+
+    System.out.println("Done!");
+  }
+}
